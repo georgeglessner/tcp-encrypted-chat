2c758cddb492e32f0f46dee44f9d4d76fea8e9f5
George Glessner <glessneg@mail.gvsu.edu>
2018-03-19 15:03:18 -0400

    Admin functions, some name changes

--- client.py
+++ client.py
@@ -1,35 +1,54 @@
-import select, socket, sys
+#!/usr/bin/env python
+
+import select
+import socket
+import sys
+
+ADMIN_PWD = 'pwd'
 
 def main():
     s = socket.socket()
-    ip_addr = raw_input("Enter an IP Adrress: ")
-    port = input("Enter a Port Number: ")
-    username = raw_input("Enter a Username: ")
+    ip_addr = raw_input('Enter an IP Adrress: ')
+    port = input('Enter a Port Number: ')
+    username = raw_input('Enter a Username: ')
 
-    s.connect((ip_addr,port))
+    s.connect((ip_addr, port))
     s.send(username)
     s.setblocking(0)
-    inout = [sys.stdin,s]
+    inout = [sys.stdin, s]
 
-    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    print "CHAT"
-    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-     
+    print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    print 'CHAT'
+    print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+
     running = 1
     while(running):
-        readable,writable,exeptional = select.select(inout,[],[])
+        readable, writable, exeptional = select.select(inout, [], [])
         for sock in readable:
             if sock == s:
                 data = sock.recv(1024)
-                if not data:
+                if data == 'close_socket':
+                    s.close()
+                    sys.exit()
+                elif not data:
                     sys.exit()
                 else:
                     print data
             else:
                 msg = raw_input()
-                final_msg = username + ": " + msg
-                s.send(final_msg)
-    s.close()
+                print msg[0:4]
+                if msg[0:5] == '$boot':
+                    admin = raw_input('Enter password: ')
+                    if admin == ADMIN_PWD:
+                        final_msg = username + ': ' + msg
+                        s.send(final_msg)
+                    else:
+                        print 'Invalid Password!'
+                else:
+                    final_msg = username + ': ' + msg
+                    s.send(final_msg)
+    # s.close()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,18 +1,29 @@
-import select, socket, sys, Queue
+#!/usr/bin/env python
+
+import select
+import socket
+import sys
+import Queue
+
+ADMIN_PWD = 'pwd'
+
 
 def broadcast_msg(command):
     temp = command.split()
-    temp[1] = temp[1].replace(temp[1][0],'[')
+    temp[1] = temp[1].replace(temp[1][0], '[')
     temp[1] = temp[1] + ']'
     swap_temp = temp[1]
     temp[1] = temp[0]
     temp[0] = swap_temp
-    return " ".join(temp)
-        
+    return ' '.join(temp)
+
+
 def main():
+    global server, ADMIN_PWD
+    ip_addr = int(raw_input('Enter a Port Number: '))
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setblocking(0)
-    server.bind(('localhost', 1234))
+    server.bind(('localhost', ip_addr))
     server.listen(5)
     inputs = [server]
     outputs = []
@@ -29,59 +40,80 @@ def main():
             if s is server:
                 connection, client_address = s.accept()
                 connection.setblocking(0)
+                print connection
                 inputs.append(connection)
                 message_queues[connection] = Queue.Queue()
                 username_flag = 1
             else:
                 data = s.recv(1024)
-                print 'Recieved: ' + str(data)
-                if username_flag:
-                    client_list.append(data)
-                    username_flag = 0
-                    break
-                if data:
-                    # If data is a command
-                    temp = data.split()
-                    if temp[1][0] == '$':
-                        # 'Show Clients' command
-                        if temp[1] == '$show_clients':
-                            data = ""
-                            data += "CLIENT LIST: " + ", ".join(client_list)
-                        # 'Broadcast' command
-                        if temp[1] == "$broadcast":
-                            count = 0
-                            for client in inputs:
-                                if count == 0:
-                                    count+=1
-                                else: 
-                                    message_queues[client].put(broadcast_msg(data))
-                                    outputs.append(client)
-                                    count+=1
-                        # Invalid command
-                        if temp[1] == 'Invalid command':
-                            data = 'Invalid Command'
-                    # message to client
-                    temp_data = data.split()
-                    if temp_data[1] == 'to':
-                        if temp_data[2] in client_list:
-                            recipient = client_list.index(temp_data[2])
-                            recipient += 1
-                            temp_data = temp_data[0] + ' '.join(temp_data[3:])
-                            message_queues[inputs[recipient]].put(temp_data)
-                            outputs.append(inputs[recipient])
-                        else:
-                            data = 'Client not in list'
-                    # Echo message
-                    elif s not in outputs:
-                        message_queues[s].put(data)
-                        outputs.append(s) 
-                # Client Disconnected
+                # don't print if client disconnects
+                if data == '':
+                    pass
                 else:
-                    if s in outputs:
-                        outputs.remove(s)
-                    inputs.remove(s)
-                    s.close()
-                    del message_queues[s]
+                    print 'Received from', str(data)
+                    if username_flag:
+                        client_list.append(data)
+                        username_flag = 0
+                        break
+                    if data:
+                        # If data is a command
+                        temp = data.split()
+                        if temp[1][0] == '$':
+                            # get client list
+                            if temp[1] == '$clients':
+                                data = ''
+                                data += 'CLIENT LIST: ' + \
+                                    ', '.join(client_list)
+                            # send broadcast message
+                            if temp[1] == '$broadcast':
+                                count = 0
+                                for client in inputs:
+                                    if count == 0:
+                                        count += 1
+                                    else:
+                                        message_queues[client].put(
+                                            broadcast_msg(data))
+                                        outputs.append(client)
+                                        count += 1
+                            if temp[1] == '$boot':
+                                if temp[2] in client_list:
+                                    recipient = client_list.index(temp[2])
+                                    recipient += 1
+                                    temp_data = 'close_socket'
+                                    message_queues[inputs[recipient]].put(
+                                        temp_data)
+                                    outputs.append(inputs[recipient])
+                                else:
+                                    data = 'Client not in list'
+                            # Invalid command
+                            if temp[1] == 'Invalid command':
+                                data = 'Invalid Command'
+                        # message to client
+                        temp_data = data.split()
+                        # send to specific client
+                        if temp_data[1] == '$sendto':
+                            if temp_data[2] in client_list:
+                                recipient = client_list.index(temp_data[2])
+                                recipient += 1
+                                temp_data = temp_data[0] + \
+                                    ' '.join(temp_data[3:])
+                                message_queues[inputs[recipient]].put(
+                                    temp_data)
+                                outputs.append(inputs[recipient])
+                            else:
+                                data = 'Client not in list'
+                        # Echo message
+                        elif s not in outputs:
+                            message_queues[s].put(data)
+                            outputs.append(s)
+                    # Client Disconnected
+                    else:
+                        if s in outputs:
+                            outputs.remove(s)
+                            client
+                        inputs.remove(s)
+                        s.close()
+                        del message_queues[s]
 
         for s in writable:
             try:
@@ -90,7 +122,7 @@ def main():
                 outputs.remove(s)
             else:
                 s.send(next_msg)
-                print 'Sent: ' + next_msg
+                print 'Sent-> ' + next_msg
 
         for s in exceptional:
             inputs.remove(s)
@@ -99,5 +131,6 @@ def main():
             s.close()
             del message_queues[s]
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
